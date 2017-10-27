46086ca10c727a569f5612e3470a5cbae69c9580
Michael Essaf <michaelessaf@D-108-179-185-135.dhcp4.washington.edu>
2017-10-27 11:24:58 -0700

    participation

--- __tests__/app.spec.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import {greet, invertCase} from '../js/app'; //import the functions to test!
-
-/* tests go here */
-
--- __tests__/index.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import * as fs from 'fs'; //import functions for working with file system
-const htmlContent = fs.readFileSync('index.html', 'utf-8'); //load the HTML content
-
-/* Your tests go here */
-
